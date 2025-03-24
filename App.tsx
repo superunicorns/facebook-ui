b47b6074d445960abe9706e5d8348387ca872f34
UnicornDad8 <unicorndad8@gmail.com>
2025-03-24 13:31:03 -0300

    Add typescript support

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Let's start Facebook UI redesign!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: "dodgerblue",
     alignItems: 'center',
     justifyContent: 'center',
   },
