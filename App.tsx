431a79afcdb3668a867f9758c510a8e4cd77fa52
UnicornDad8 <unicorndad8@gmail.com>
2025-03-25 09:57:44 -0300

    Make CircleIcon a reusable component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import CircularIcon from './components/CircularIcon';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CircularIcon />
+      <CircularIcon backgroundColor='#BEBEBE' icon={ <MaterialIcons name="search" size={20} color="#fff" /> } />
+      <CircularIcon backgroundColor='#FA7373' icon={ <MaterialCommunityIcons name="bell" size={18} color="#fff" /> } />
+      <CircularIcon backgroundColor='#84B6FB' notification icon={ <MaterialCommunityIcons name="account-group" size={18} color="#fff" /> } />
+      <CircularIcon backgroundColor='blue' icon={ <MaterialIcons name="search" size={18} color="#fff" /> } />
       <StatusBar style="auto" />
     </View>
   );
--- components/CircularIcon.tsx
+++ components/CircularIcon.tsx
@@ -1,18 +1,39 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Feather from '@expo/vector-icons/Feather';
 
-function CircularIcon() {
+function CircularIcon({ 
+  backgroundColor, 
+  icon, 
+  notification }: {
+    backgroundColor: string; 
+    icon: ReactNode;
+    notification?: boolean;
+  }) {
   return (
     <View style={{
-      backgroundColor: "#BEBEBE",
+      backgroundColor,
       width: 40,
       height: 40,
       borderRadius: 20,
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
+      position: "relative"
     }}>
-      <Feather name="search" size={18} color="#fff" />
+      {notification && <View style={{
+        backgroundColor: "red",
+        width: 12,
+        height: 12,
+        borderRadius: 6,
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: -2,
+        right: 2,
+        borderColor: "#fff",
+        borderWidth: 2
+      }} />}
+      {icon}
     </View>
   )
 }
