b0f06770543015448754f90ba46a003b6f8d0508
UnicornDad8 <unicorndad8@gmail.com>
2025-03-25 10:28:53 -0300

    Organize CircularIcon styles

--- components/CircularIcon.tsx
+++ components/CircularIcon.tsx
@@ -1,6 +1,5 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React, { ReactNode } from 'react'
-import Feather from '@expo/vector-icons/Feather';
 
 function CircularIcon({ 
   backgroundColor, 
@@ -11,31 +10,35 @@ function CircularIcon({
     notification?: boolean;
   }) {
   return (
-    <View style={{
-      backgroundColor,
-      width: 40,
-      height: 40,
-      borderRadius: 20,
-      alignItems: "center",
-      justifyContent: "center",
-      position: "relative"
-    }}>
-      {notification && <View style={{
-        backgroundColor: "red",
-        width: 12,
-        height: 12,
-        borderRadius: 6,
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
-        top: -2,
-        right: 2,
-        borderColor: "#fff",
-        borderWidth: 2
-      }} />}
+    <View style={[{ backgroundColor }, styles.container]}>
+      {notification && <View style={styles.notification} />}
       {icon}
     </View>
   )
 }
 
 export default CircularIcon;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative"
+  },
+  notification: {
+    backgroundColor: "red",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: -2,
+    right: 2,
+    borderColor: "#fff",
+    borderWidth: 2
+  },
+})
---
+++ components/HeaderBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HeaderBar = () => {
+  return (
+    <View>
+      <Text>Header Bar</Text>
+    </View>
+  )
+}
+
+export default HeaderBar
